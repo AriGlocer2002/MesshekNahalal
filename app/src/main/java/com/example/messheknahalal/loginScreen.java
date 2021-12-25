9d06003ab966e6e608f45eff775abe4ae9cac827
ariog <ariogl02@gmail.com>
2021-12-25 20:43:27 +0200

    20:43

--- app/src/main/java/com/example/messheknahalal/loginScreen.java
+++ app/src/main/java/com/example/messheknahalal/loginScreen.java
@@ -1,4 +1,16 @@
 package com.example.messheknahalal;
 
-public class loginScreen {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class loginScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
 }
--- app/src/main/java/com/example/messheknahalal/splashScreen.java
+++ app/src/main/java/com/example/messheknahalal/splashScreen.java
@@ -2,16 +2,40 @@ package com.example.messheknahalal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class splashScreen extends AppCompatActivity {
 
-
-    // Hola me llamo ari
+    ImageView img_logo1;
+    Animation logoAnim;
+    Intent intent;
+    private static int LOGINSCREEN = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
+
+        img_logo1 = findViewById(R.id.img_logo1);
+        logoAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        img_logo1.setAnimation(logoAnim);
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                intent = new Intent(splashScreen.this, loginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },LOGINSCREEN);
+
     }
 }
