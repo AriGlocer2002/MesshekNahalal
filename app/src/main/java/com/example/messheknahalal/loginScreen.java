e9cf0c4b441e15296d2f156788d12a9459846ab2
ariog <ariogl02@gmail.com>
2021-12-26 04:00:34 +0200

    4:00 Israel last update

--- app/src/main/java/com/example/messheknahalal/loginScreen.java
+++ app/src/main/java/com/example/messheknahalal/loginScreen.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +15,8 @@ public class loginScreen extends AppCompatActivity {
     EditText et_email_login, et_password_login;
     Button btn_login1;
     Intent intent;
+    ImageView iv_newPassword;
+    TextView tv_newPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,8 @@ public class loginScreen extends AppCompatActivity {
         et_email_login = findViewById(R.id.et_email_login);
         et_password_login = findViewById(R.id.et_password_login);
         btn_login1 = findViewById(R.id.btn_login1);
+        iv_newPassword = findViewById(R.id.iv_newPassword);
+        tv_newPassword = findViewById(R.id.tv_newPassword);
 
 
         btn_login1.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/messheknahalal/signUpScreen.java
+++ app/src/main/java/com/example/messheknahalal/signUpScreen.java
@@ -1,15 +1,42 @@
 package com.example.messheknahalal;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class signUpScreen extends AppCompatActivity {
+
+    EditText et_name_signUp, et_last_name_signUp, et_id_number_signUp, et_phone_number_signUp,
+            et_email_address_signUp,  et_password_signUp, et_confirm_password_signUp, et_admin_code_signUp;
+    Button btn_login2, btn_signUp2;
+    CheckBox cb_admin;
+    ImageView iv_profile_pic;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        et_name_signUp = findViewById(R.id.et_name_signUp);
+        et_last_name_signUp = findViewById(R.id.et_last_name_signUp);
+        et_id_number_signUp = findViewById(R.id.et_id_number_signUp);
+        et_phone_number_signUp = findViewById(R.id.et_phone_number_signUp);
+        et_email_address_signUp = findViewById(R.id.et_email_address_signUp);
+        et_password_signUp = findViewById(R.id.et_password_signUp);
+        et_confirm_password_signUp = findViewById(R.id.et_confirm_password_signUp);
+        et_admin_code_signUp = findViewById(R.id.et_admin_code_signUp);
+
+        cb_admin = findViewById(R.id.cb_admin);
+
+        btn_login2 = findViewById(R.id.btn_login2);
+        btn_signUp2 = findViewById(R.id.btn_signUp2);
+
+        iv_profile_pic = findViewById(R.id.iv_profile_pic);
     }
 }
