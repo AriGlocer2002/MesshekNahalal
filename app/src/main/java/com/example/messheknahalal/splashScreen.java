ca256dcb6b443fc37944006cb036a4efa926349c
ariog <ariogl02@gmail.com>
2021-12-30 00:56:22 +0200

    00:56 30/12/2021 last update

--- app/src/main/java/com/example/messheknahalal/splashScreen.java
+++ app/src/main/java/com/example/messheknahalal/splashScreen.java
@@ -9,6 +9,9 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class splashScreen extends AppCompatActivity {
 
     ImageView img_logo1;
@@ -16,12 +19,18 @@ public class splashScreen extends AppCompatActivity {
     Intent intent;
     private static int LOGINSCREEN = 5500;
 
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("Admin").child("Person_0002").child("password");
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
 
-
+        myRef.setValue("888888");
 
         img_logo1 = findViewById(R.id.img_logo1);
         logoAnim = AnimationUtils.loadAnimation(this, R.anim.fade_in);
