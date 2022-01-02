c398763d4ff5f650f809f6026cf5328b5428034c
ariog <ariogl02@gmail.com>
2022-01-03 00:09:31 +0200

    00:09 3/1/2022 last update

--- app/src/main/java/com/example/messheknahalal/mainScreen.java
+++ app/src/main/java/com/example/messheknahalal/mainScreen.java
@@ -8,11 +8,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -33,12 +33,15 @@ public class mainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
 
+
+        //bottom navigation bar
         bottomNav = findViewById(R.id.bottomNav);
 
         bottomNav.setOnNavigationItemSelectedListener(bottomNavMethod);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new HomeFragment()).commit();
 
 
+        //navigation bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setContentView(toolbar);
 
@@ -50,7 +53,6 @@ public class mainScreen extends AppCompatActivity {
         toggle.syncState();
 
     }
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
             BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -67,15 +69,11 @@ public class mainScreen extends AppCompatActivity {
                         case R.id.products:
                             fragment = new ProductsFragment();
                             break;
-
                     }
-
                     getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
-
                     return true;
                 }
             };
-
 
     public void onBackPressed(){
         if(drawer.isDrawerOpen(GravityCompat.START)){
